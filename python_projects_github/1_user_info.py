5fd0351428bd5685991efba788502b64b6fc24c8
LukysYeep <7lukyproch@gmail.com>
2025-07-13 02:34:29 +0000

    Bugfixed 1_user_info.py

--- python_projects_github/1_user_info.py
+++ python_projects_github/1_user_info.py
@@ -1,14 +1,14 @@
-first_name = input("enter your first name:") # lets you input your first name
-print("your first name is ", first_name.capitalize()) # outputs a string and a variable first_name but capitalized
+first_name = input("enter your first name: ") # lets you input your first name
+print("your first name is", first_name.capitalize()) # outputs a string and a variable first_name but capitalized
 
-surname = input("enter your surname:") # lets you input your surname
-print("your surname is ", surname.capitalize()) # outputs a string and a variable surname but capitalized
+surname = input("enter your surname: ") # lets you input your surname
+print("your surname is", surname.capitalize()) # outputs a string and a variable surname but capitalised
 
-phone_number = input("enter your phone number:") # lets you input a phone number
-print('your number is ', phone_number) # outputs a string and a variable phonenumber
+phone_number = input("enter your phone number: ") # lets you input a phone number
+print('your number is', phone_number) # outputs a string and a variable phone_number
 
-first_name = name.capitalize()
-surname = username.capitalize()
+first_name = first_name.capitalize()
+surname = surname.capitalize()
 # ↑↑↑ Capitalizing variables first_name and surname as shown above
 
 def hello(firstname, surname): # defines a function hello
@@ -16,6 +16,6 @@ def hello(firstname, surname): # defines a function hello
     print(f"Hi, {firstname} {surname}") # outputs a string "Hi, " with variables first_name and surname injected in the string
 
 
-hello(firstname, surname) # calls the function hello
+hello(first_name, surname) # calls the function hello
 
 
